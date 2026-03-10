4a2fafbdab2eebe0d1369b217c8d46c983e54bb2
Mohamed Waleed <mw99746@gmail.com>
2026-03-11 00:03:39 +0200

    feat: add file validators - `checkFileSize`   - checks file size by comparing the file size from input file array and max size - `isFileUploaded` checks if file is uploaded successfully - `validateFileType` checks if the file type is allowed

--- src/Classes/Validators.php
+++ src/Classes/Validators.php
@@ -4,26 +4,48 @@ namespace Src\Classes;
 
 class Validators
 {
-    static function  stringValidator($str, $min, $max):bool
+    static function stringValidator($str, $min, $max): bool
     {
         return (strlen($str) < $min || strlen($str) > $max);
     }
-    static function emailValidator($email):bool
+
+    static function emailValidator($email): bool
     {
-       return !filter_var($email, FILTER_VALIDATE_EMAIL);
+        return !filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
-    static function numberValidator ($number):bool
+    static function numberValidator($number): bool
     {
-     return is_numeric($number);
+        return is_numeric($number);
     }
-    static function passwordMatchConfirmPassword($password, $confirmPassword):bool
+
+    static function passwordMatchConfirmPassword($password, $confirmPassword): bool
     {
-       return ($password !== $confirmPassword);
+        return ($password !== $confirmPassword);
     }
+
     static function passwordValidator($password): bool
     {
         $pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
         return (!preg_match($pattern, $password));
+    }
+
+    static function checkFileSize(array $file, $maxSize): bool
+    {
+        return ($file["size"] > $maxSize);
+    }
+
+    static function isFileUploaded(array $file): bool
+    {
+        return (isset($file["error"]) && $file["error"] === UPLOAD_ERR_OK);
+    }
+
+    static function validateFileType(array $file): bool
+    {
+        $allowedTypes = ["images/jpeg", 'images/png'];
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $file['tmp_name']);
+        finfo_close($finfo);
+        return in_array($mimeType, $allowedTypes);
     }
 }
